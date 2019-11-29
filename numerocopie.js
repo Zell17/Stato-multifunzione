20a6e8eaaef07a88606ba3d005936b485513fc95
Marco Natella <33099103+Zell17@users.noreply.github.com>
2019-11-29 17:17:11 +0100

    Add files via upload

--- numerocopie.js
+++ numerocopie.js
@@ -21,52 +21,42 @@ var sharpNOtoner = ["Modello dispositivo: MX-M453N", "Modello dispositivo: MX-M2
 var sharpBKtoner = ["Modello dispositivo: MX-M354N", "Modello dispositivo: MX-M565N", "Modello dispositivo: MX-M564N", "Modello dispositivo: MX-M266N", "Modello dispositivo: MX-M314N", "Modello dispositivo: MX-M315N", "Modello dispositivo: MX-M316N"];
 
 //controllo data
+var nomemese = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+var now = new Date();
+var mesecorrente = nomemese[now.getUTCMonth()];
+console.log(mesecorrente)
 
-const mese = new Date();
-var mesecorrente = mese.getUTCMonth();
+// controllo mese precedente
+var nummeseprecedente = (now.getUTCMonth() - 1);
+var meseprecedente = nomemese[nummeseprecedente];
+console.log(meseprecedente)
 
-if (mesecorrente == 0) {
-    mesecorrente = 'Jan'
-} else
-    if (mesecorrente == 1) {
-        mesecorrente = 'Feb'
-    } else
-        if (mesecorrente == 2) {
-            mesecorrente = 'Mar'
-        } else
-            if (mesecorrente == 3) {
-                mesecorrente = 'Apr'
-            } else
-                if (mesecorrente == 4) {
-                    mesecorrente = 'May'
-                } else
-                    if (mesecorrente == 5) {
-                        mesecorrente = 'Jun'
-                    } else
-                        if (mesecorrente == 6) {
-                            mesecorrente = 'Jul'
-                        } else
-                            if (mesecorrente == 7) {
-                                mesecorrente = 'Aug'
-                            } else
-                                if (mesecorrente == 8) {
-                                    mesecorrente = 'Sep'
-                                } else
-                                    if (mesecorrente == 9) {
-                                        mesecorrente = 'Oct'
-                                    } else
-                                        if (mesecorrente == 10) {
-                                            mesecorrente = 'Nov'
-                                        } else
-                                            if (mesecorrente == 11) {
-                                                mesecorrente = 'Dic'
-                                            };
 
 const anno = new Date();
 var annocorrente = anno.getUTCFullYear();
 var appoggio = [];
+var comunicaminpre = 0;
+var comunicamaxpre = 0;
+
+for (let i = 0, max = data.length; i < max; i++) {
+    appoggio[i] = data[i].split(' ')
+    if ((appoggio[i][3]) == meseprecedente && (appoggio[i][6]) == annocorrente && comunicaminpre == 0) {
+        comunicaminpre = i
+    };
+};
+
+for (let i = 0, max = data.length; i < max; i++) {
+    if ((appoggio[i][3]) == meseprecedente && (appoggio[i][6]) == annocorrente && i >= comunicamaxpre) {
+        comunicamaxpre = i
+    }
+};
+console.log(comunicamaxpre, comunicaminpre);
+
+// controllo comunicazioni mese corrente 
+
 var comunicamin = 0;
 var comunicamax = 0;
+
 
 for (let i = 0, max = data.length; i < max; i++) {
     appoggio[i] = data[i].split(' ')
@@ -80,6 +70,8 @@ for (let i = 0, max = data.length; i < max; i++) {
         comunicamax = i
     }
 };
+
+//controllo sharp202
 for (let i = 0, max = modello.length; i < max; i++) {
     if ((modello[i]) == "Modello dispositivo: SHARP MX-M202D") {
         conteggioTotCo.splice(i, 0, 'Non comunicato')
@@ -90,9 +82,14 @@ for (let i = 0, max = modello.length; i < max; i++) {
 
 document.write(mesecorrente + "   " + annocorrente + "<br>" + "<br>");
 
-for (let i = comunicamin, max = comunicamax; i < max; i++) {
-
-    document.write(`${i}` + mittente[i] + " -------- " + modello[i] + " -------- " + conteggioTotBN[i] + "<br>");
-    document.write(`${i}` + mittente[i] + " -------- " + modello[i] + " -------- " + conteggioTotCo[i] + "<br>");
-               document.write("<br>" + "<br>");
-    };
+for (let i = (comunicamax - 1), max = comunicamin; i > max; i--) {    
+    document.write(`${i}`+ "--" + mittente[i] + " -------- " + modello[i] + " -------- " + conteggioTotBN[i] + "<br>");
+    document.write(`${i}`+ "--" + mittente[i] + " -------- " + modello[i] + " -------- " + conteggioTotCo[i] + "<br>");
+    for (let k = (comunicamaxpre - 1), max = comunicaminpre; k > max; k--) {
+        if (mittente[i] == mittente[k] && modello[i] == modello[k]) {
+            document.write(`${k}` + "--" + mittente[k] + " -------- " + modello[k] + " -------- " + conteggioTotBN[k] + "<br>");
+            document.write(`${k}`+ "--" + mittente[k] + " -------- " + modello[k] + " -------- " + conteggioTotCo[k] + "<br>");
+        } 
+    }
+    document.write("<br>" + "<br>");
+};
